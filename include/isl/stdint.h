bc0c3006782ada28b2445ae6008a464572de9f08
Andrei Lascu <al2510@imperial.ac.uk>
2018-07-03 16:33:55 +0200

    Updated isl
    Due to having to regenerate "isl-noexceptions.h" file. Should not need
    to update it further.
    
    Repo: github.com/tobig/isl.git
    Commit: 7dc90f05d1678261e41f97e8fae62f8faeb1396
    Branch: polly-bindings-andrei

--- include/isl/stdint.h
+++ include/isl/stdint.h
@@ -2,7 +2,7 @@
 #define _ISL_INCLUDE_ISL_STDINT_H 1
 #ifndef _GENERATED_STDINT_H
 #define _GENERATED_STDINT_H "isl 0.19"
-/* generated using gnu compiler gcc (GCC) 7.3.1 20180312 */
+/* generated using gnu compiler gcc (GCC) 8.1.1 20180531 */
 #define _STDINT_HAVE_STDINT_H 1
 #include <stdint.h>
 #endif
