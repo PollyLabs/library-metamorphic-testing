618571cb9603c04388d2ee0ae7afbdd620ea7918
Andrei Lascu <al2510@imperial.ac.uk>
2018-08-24 16:04:20 +0100

    Added omega config files and library files

---
+++ include/testing/omega-gen-helpers.h
@@ -0,0 +1,38 @@
+#include <random>
+#include "omega.h"
+
+int
+getRandInt(std::mt19937& rng, int min, int max)
+{
+    return std::uniform_int_distribution<int>(min, max)(rng);
+}
+
+omega::Relation
+makeTupleRelation(int first, int second)
+{
+    omega::Relation rel_out(2);
+    omega::F_And* f_and = rel_out.add_and();
+    omega::EQ_Handle eq = f_and->add_EQ();
+    eq.update_coef(rel_out.input_var(1), 1);
+    eq.update_const(-1 * first);
+    eq = f_and->add_EQ();
+    eq.update_coef(rel_out.input_var(2), 1);
+    eq.update_const(-1 * second);
+    return rel_out;
+}
+
+omega::Relation
+makeRelation(unsigned int seed, unsigned int dim_cnt)
+{
+    std::mt19937 rng(seed);
+    omega::Relation rel_out = omega::Relation(dim_cnt);
+    omega::F_And* f_and = rel_out.add_and();
+    for (int i = 0; i < rel_out.n_set(); ++i)
+    {
+        omega::EQ_Handle eq = f_and->add_EQ();
+        eq.update_const(-1 * getRandInt(rng, 1, 10));
+        eq.update_coef(rel_out.input_var(i + 1), 1);
+    }
+    return rel_out;
+}
+
