5cb2d0ca15b6588a39d3a2fc7992fb55519ba2fa
Andrei Lascu <al2510@imperial.ac.uk>
2018-09-26 15:20:41 +0100

    Fix constraint count bug for other stat file

--- include/testing/isl-meta-stats.h
+++ include/testing/isl-meta-stats.h
@@ -1,11 +1,11 @@
-#pragma once
+#ifndef ISL_META_STATS_HPP
+#define ISL_META_STATS_HPP
 
 static unsigned int c_cnt;
 
 isl::stat
 getBSConstraints(isl::basic_set bs)
 {
-    extern unsigned int c_cnt;
     c_cnt += bs.n_constraint();
     return isl::stat::ok;
 }
@@ -13,6 +13,8 @@ getBSConstraints(isl::basic_set bs)
 void
 printStats(isl::set s)
 {
+    c_cnt = 0;
+
 	fprintf(stderr, "DIM SET = %d\n", s.dim(isl::dim::set));
 	fprintf(stderr, "DIM PARAM = %d\n", s.dim(isl::dim::param));
 	fprintf(stderr, "SET EMPTY = %s\n", s.is_empty() ? "true" : "false");
@@ -20,3 +22,5 @@ printStats(isl::set s)
     s.foreach_basic_set(getBSConstraints);
     fprintf(stderr, "N CONSTRAINTS = %d\n", c_cnt);
 }
+
+#endif
