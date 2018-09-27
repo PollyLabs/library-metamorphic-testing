ac85b2ba24c181b4e1b61c7fde29db76165e07ea
Andrei Lascu <al2510@imperial.ac.uk>
2018-09-27 16:31:17 +0100

    Update path to default config file in test_emitter

--- src/test_emitter.cpp
+++ src/test_emitter.cpp
@@ -2,7 +2,7 @@
 
 static unsigned int indent = 0;
 const std::string config_file_path =
-    "/home/sentenced/Documents/Internships/2018_ETH/work/sets/config_files/config.yaml";
+    "/home/sentenced/Documents/Internships/2018_ETH/work/sets/config_files/config_isl.yaml";
 
 const bool DEBUG = false;
 //const bool DEBUG = true;
