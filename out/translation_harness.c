33285af494657b8dd9906ff204827ca9221d5cfa
Andrei Lascu <al2510@imperial.ac.uk>
2018-09-28 14:20:37 +0100

    Added wrapper for translation content

---
+++ out/translation_harness.c
@@ -0,0 +1,13 @@
+#include <isl/ctx.h>
+#include <isl/space.h>
+#include <isl/local_space.h>
+#include <isl/aff.h>
+#include <isl/set.h>
+
+int main()
+{
+    isl_ctx *ctx_0 = isl_ctx_alloc();
+    {
+#include <translate_content.c>
+    }
+}
