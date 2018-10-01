85ddafd2ce3a06c0463f26dcbfda89f70637ec80
Andrei Lascu <al2510@imperial.ac.uk>
2018-10-01 16:52:06 +0100

    Revert paths in "compile_isl"

--- out/compile_isl.sh
+++ out/compile_isl.sh
@@ -2,8 +2,8 @@
 if [ $# -eq 1 ]
 then
     echo "Compiling $1"
-    g++ -std=c++11 -o ${1%.*} $1 -I./include -L./libs -lisl
+    g++ -std=c++11 -o ${1%.*} $1 -I../include -L../libs -lisl
 else
-    echo "Compiliing test.cpp"
-    g++ -std=c++11 -o test test.cpp -I./include -L./libs/ -lisl
+    echo "Compiling test.cpp"
+    g++ -std=c++11 -o test test.cpp -I../include -L../libs/ -lisl
 fi
