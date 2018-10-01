d76b5d060d8ff665dc46146230fe86537eb8c6e3
Andrei Lascu <al2510@imperial.ac.uk>
2018-10-01 14:04:01 +0100

    Add output to compilation script

--- out/compile_isl.sh
+++ out/compile_isl.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 if [ $# -eq 1 ]
 then
-    g++ -Wall -Wextra -std=c++11 -o ${1%.*} $1 -I../include -L../libs -lisl
+    echo "Compiling $1"
+    g++ -std=c++11 -o ${1%.*} $1 -I./include -L./libs -lisl
 else
-    g++ -Wall -Wextra -std=c++11 -o test test.cpp -I../include -L../libs/ -lisl
+    echo "Compiliing test.cpp"
+    g++ -std=c++11 -o test test.cpp -I./include -L./libs/ -lisl
 fi
