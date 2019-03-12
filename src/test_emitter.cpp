d5bcaec4a802c5883ad9e026f47666c6f2a8ef69
Andrei Lascu <al2510@imperial.ac.uk>
2019-03-12 14:34:43 +0000

    Further updates and strtk integration
    * `getFuncBySignature` now takes a full signature, rather than only
    parameter types
    * Added integer subtraction helper function for strtk
    * By adding the `--debug` flag to `test_emitter` in a previous commit,
    now can set value of `DEBUG` to `false`

--- src/test_emitter.cpp
+++ src/test_emitter.cpp
@@ -1,12 +1,11 @@
 #include "test_emitter.hpp"
 
 static unsigned int indent = 0;
+const std::string default_config_file =
+    "/home/sentenced/Documents/Internships/2018_ETH/work/sets/config_files/config_isl.yaml";
 
-const std::string config_file_path =
-    "/home/dan/workspace/library-metamorphic-testing/config_files/config_gmp.yaml";
-
-//bool DEBUG = false;
-bool DEBUG = true;
+bool DEBUG = false;
+//bool DEBUG = true;
 bool META_TESTING = true;
 
 std::map<std::string, Modes> string_to_mode {
