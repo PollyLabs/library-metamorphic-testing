4c54a74d2b675d034ae2fbd31e5d9b12df2563f8
Andrei Lascu <al2510@imperial.ac.uk>
2019-04-01 17:10:27 +0100

    Fix include path in `compile_z3`

--- scripts/compile/compile_z3.sh
+++ scripts/compile/compile_z3.sh
@@ -2,8 +2,8 @@
 if [ $# -eq 1 ]
 then
     echo "Compiling $1"
-    g++ -g -std=c++11 -g -o ${1%.*} $1 -I../include_local -lz3
+    g++ -g -std=c++11 -g -o ${1%.*} $1 -I../include -lz3
 else
     echo "Compiling test.cpp"
-    g++ -g -std=c++11 -g -o test test.cpp -I../include_local -lz3
+    g++ -g -std=c++11 -g -o test test.cpp -I../include -lz3
 fi
