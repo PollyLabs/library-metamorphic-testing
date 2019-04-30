879e8fcd1e5e8a549e842d62f444e7074ce23998
Pritam Gharat <pgharat@rotherham.doc.ic.ac.uk>
2019-04-30 13:44:04 +0100

    Updated API

--- include/testing/eigen-matrix-helpers.h
+++ include/testing/eigen-matrix-helpers.h
@@ -1,7 +1,31 @@
-#ifndef GMP_HELPERS
-#define GMP_HELPERS
+#ifndef EIGEN_MATRIX_HELPERS
+#define EIGEN_MATRIX_HELPERS
 
 #include "Eigen/Dense"
 
+Eigen::MatrixXf getIdentity()
+{
+	Eigen::MatrixXf res(3,3);
+
+	for(unsigned int i=0; i < 3; i++)
+	{
+		for(unsigned int j=0; j < 3; j++)
+		{
+			res(i,j) = 1;
+		}
+	}
+
+	return res;
+}
+
+int getMinusOne()
+{
+	return -1;
+}
+
+int getInteger(int x)
+{
+	return x;
+}
 
 #endif
--- scripts/compile/compile_eigen.sh
+++ scripts/compile/compile_eigen.sh
@@ -2,8 +2,8 @@
 if [ $# -eq 1 ]
 then
     echo "Compiling $1"
-    g++ -std=c++11 -g -o ${1%.*} $1 -I../include/eigen
+    g++ -std=c++11 -g -o ${1%.*} $1 -I../include/eigen -I../include
 else
     echo "Compiling test.cpp"
-    g++ -std=c++11 -g -o test test.cpp -I../include/eigen
+    g++ -std=c++11 -g -o test test.cpp -I../include/eigen -I../include
 fi
