7f4dc31f1a2cbb568c6749bbb03e9c594691a864
PritamMG <pgharat@rotherham.doc.ic.ac.uk>
2019-06-20 15:25:40 +0100

    Creating Docker Files

--- scripts/compile/compile_eigen.sh
+++ scripts/compile/compile_eigen.sh
@@ -2,8 +2,8 @@
 if [ $# -eq 1 ]
 then
     echo "Compiling $1"
-    g++ -std=c++11 -g -o ${1%.*} $1 -I../include/eigen -I../include
+    g++ -std=c++11 -g -o ${1%.*} $1 -I/data/pgharat/project3/library-metamorphic-testing/include/eigen -I/data/pgharat/project3/library-metamorphic-testing/include
 else
     echo "Compiling test.cpp"
-    g++ -std=c++11 -g -o test test.cpp -I../include/eigen -I../include
+    g++ -std=c++11 -g -o test test.cpp -I/data/pgharat/project3/library-metamorphic-testing/include/eigen -I/data/pgharat/project3/library-metamorphic-testing/include
 fi
