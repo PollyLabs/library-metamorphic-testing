b5f661851b0c9435e460f76032758092c7ef7728
Andrei Lascu <al2510@imperial.ac.uk>
2021-06-01 10:51:52 +0100

    MF++R update

--- src/clang_interface.cpp
+++ src/clang_interface.cpp
@@ -187,8 +187,8 @@ generateObjectInstructions(std::string type_name, std::string indent)
     const ApiType* new_obj_type = getFuzzer()->getTypeByName(type_name);
     const ApiObject* new_obj = getFuzzer()->generateNewObject(new_obj_type);
     std::vector<std::string> instrs_str_vec = getFuzzer()->getInstrStrs();
-    instrs_str_vec.push_back("// END FUZZING");
-    instrs_str_vec.insert(instrs_str_vec.begin(), "// START FUZZING");
+    instrs_str_vec.insert(instrs_str_vec.begin(), "fuzz::mfr_fuzz_start(\"" + new_obj->toStr() + "\");");
+    instrs_str_vec.push_back("fuzz::mfr_fuzz_end(\"" + new_obj->toStr() + "\");");
     std::string instrs_str = std::accumulate(std::begin(instrs_str_vec),
         std::end(instrs_str_vec), std::string(),
         [indent](std::string acc, std::string nxt)
